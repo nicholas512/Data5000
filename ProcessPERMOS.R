fcf9fc3a2748d68428009be931508476ee4851cc
nickbrown <nicholas512@gmail.com>
2016-04-02 14:30:58 -0400

    working version

--- ProcessPERMOS.R
+++ ProcessPERMOS.R
@@ -1,38 +1,24 @@
-source("GTN_Process.R")
-
-setwd("Q:\\CommonData\\PermafrostData\\PERMOS")
-
-dat = read.csv("Q:\\CommonData\\PermafrostData\\PERMOS\\ATT_0108_2005-01-01_2016-03-16.csv",skip=4,header=T)
-
 PERMOS2GTop <- function(filename){
   NPraw = read.csv(filename,
-                   sep=",", header=T,colClasses="character",skip=4)
-  if (sum(dim(NPraw)) == 1){
+                   sep=",", header=T,colClasses="character", skip=4)
+  
+  # A few checks
+  if (sum(dim(NPraw)) == 1 | length(grep('_Metadata', filename))==1) {
     return(NULL)
   }
   
-  outfile = sub("_data\\.","_obs\\.",filename)
-  in.Date = as.data.frame(str_match(NPraw[,1],"(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})"))
-  colnames(in.Date)=c("str","year","month","day","hour","minute","second")
+  # Define output file name
+  outfile <- sub("(.csv$)","_obs.csv", filename)
+
   
-  Date = paste(as.character(in.Date$day),"/",
-               as.character(in.Date$month),"/",
-               in.Date$year," ",as.character(in.Date$hour),":",as.character(in.Date$minute),
-               sep="")
-  Depths = names(NPraw)[-1]
-  re = regexpr("\\d\\.\\d*$",Depths)
-  Depths_mm = as.numeric(regmatches(Depths,re))*1000
+  # Put headers in body to get good names
+  Depths <- names(NPraw)[-1]
+  re <- regexpr("\\d\\.?\\d?$", Depths)
+  Depths_mm <- as.numeric(regmatches(Depths, re))*1000
   
   top = c("Date",Depths_mm)
-  
-  fix999 = setNA(-999)
-  fix888 = setNA(-888)
-  
-  data = as.data.frame(NPraw[,-1])
-  data = apply(data,2,function(x) fix999(as.numeric(x)))
-  data = apply(data,2,function(x) fix888(as.numeric(x)))
-  
-  out.data = data.frame(Date,data)
-  out = rbind(top,out.data)
+
+  out <- rbind(top,NPraw)
   write.table(out,file=outfile,quote=F,sep=",",row.names=F,col.names=F)
 }
+
